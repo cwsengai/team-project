2fee5ed8a484b6cbe0bc0eda912a5acc10e6a821
Charlotte Wong <charlottewong129@gmail.com>
2025-11-24 00:27:35 -0500

    merge CompanyDetailPage and CompanyPage

---
+++ src/main/java/framework_and_driver/ChartViewAdapter.java
@@ -0,0 +1,23 @@
+package framework_and_driver;
+
+import entity.ChartViewModel;
+
+public class ChartViewAdapter extends ChartWindow {
+
+    private final CompanyPage companyPage;
+
+    public ChartViewAdapter(CompanyPage companyPage) {
+        this.companyPage = companyPage;
+    }
+
+    @Override
+    public void updateChart(ChartViewModel vm) {
+        companyPage.updateChart(vm);
+    }
+
+    @Override
+    public void displayError(String message) {
+        companyPage.displayError(message);
+    }
+}
+
