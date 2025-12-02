e359260a69be052ccecf5248de6b6cd231aa0dfc
mirror <mirror.chen@mail.utoronto.ca>
2025-12-01 20:17:00 -0500

    checkstyle

--- src/main/java/interfaceadapter/view_model/CompanyViewModel.java
+++ src/main/java/interfaceadapter/view_model/CompanyViewModel.java
@@ -2,12 +2,12 @@ package interfaceadapter.view_model;
 
 public class CompanyViewModel {
 
-    public String name;
-    public String symbol;
-    public String sector;
-    public String industry;
-    public String description;
-    public String error;
+    private String name;
+    private String symbol;
+    private String sector;
+    private String industry;
+    private String description;
+    private String error;
 
     private Runnable listener;
 
@@ -15,7 +15,60 @@ public class CompanyViewModel {
         this.listener = listener;
     }
 
+    /**
+     * Notifies the listener of property changes.
+     */
     public void notifyListener() {
-        if (listener != null) listener.run();
+        if (listener != null) {
+            listener.run();
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
     }
 }
